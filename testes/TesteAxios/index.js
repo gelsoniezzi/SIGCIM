5534c8a14d58db546696213e8eb4042313d7b723
Gelson Iezzi <gimg@live.com>
2019-12-18 08:05:39 -0300

    Testes

--- testes/TesteAxios/index.js
+++ testes/TesteAxios/index.js
@@ -1,15 +1,24 @@
 const app = require('express')()
-const bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+var exphbs = require('express-handlebars')
 
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-const hbs = exphbs.create({ extname: 'hbs', layoutsDir: 'views', defaultLayout: null, });
+const hbs = exphbs.create({ defaultLayout: 'main', extname: 'hbs', layoutsDir: 'views/layouts'});
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-
+app.get('/', (req, res) => {
+  var insumos = [
+    {descricao: "Toalha", valor: 15.50},
+    {descricao: "Barbeador", valor: 0.50},
+    {descricao: "Moto", valor: 1015.50},
+  ]
+  res.render('insumos', {insumos: insumos})
+})
 
 app.get('/insumos', (req, res) => {
   res.render('insumos')
